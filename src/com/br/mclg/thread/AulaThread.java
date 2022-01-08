2ff30449377ce1c4065bb8abdcf76ca88c52eb09
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2022-01-07 21:22:27 -0300

    Criando a primeira Threas.

--- src/com/br/mclg/thread/AulaThread.java
+++ src/com/br/mclg/thread/AulaThread.java
@@ -1,17 +1,34 @@
 package com.br.mclg.thread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
-		/*Código da rotina para execução em paralelo*/
-		for (int posicao = 0; posicao < 10; posicao++) {
-			
-			Thread.sleep(1000);//dá um tempo
-			
-			//executando esse envio com um tempo de parada ou tempo determinado.
-			System.out.println("Executando uma rotina, envio de e-mail.");
-		}
-		System.out.println("Fim do processo de teste de Thread!!!");
-	}
+		// Thread em processamento em paralelo do envio de email
+		new Thread() {
 
+			public void run() {
+
+				// Código da rotina para execução em paralelo
+				for (int posicao = 0; posicao < 10; posicao++) {
+
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} // dá um tempo
+
+					// executando esse envio com um tempo de parada ou tempo determinado.
+					System.out.println("Executando uma rotina, envio de e-mail.");
+				}
+				// Fim do código em paralelo
+			}
+		}.start();// Start liga a thread que fica processando paralelamente por trás.
+
+		// Código do sistema do usuário continua o fluxo de trabalho
+		System.out.println("FIM DA EXECUÇÂO DA THREAD!!!");
+		// Fluxo do sistema, cadastro de venda, uma emissão de nota fiscal
+		JOptionPane.showMessageDialog(null, "Sistema em execução para o usuário.");
+	}
 }
