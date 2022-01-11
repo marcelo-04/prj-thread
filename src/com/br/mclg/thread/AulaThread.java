b89b1a44200132e10b7ab7aa4d56a4ef41100fee
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2022-01-11 20:44:06 -0300

    Thread em objeto e o Runnable

--- src/com/br/mclg/thread/AulaThread.java
+++ src/com/br/mclg/thread/AulaThread.java
@@ -5,54 +5,58 @@ import javax.swing.JOptionPane;
 public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		// Thread em processamento em paralelo do envio de email
-		new Thread() {
-
-			public void run() {
-
-				// Código da rotina para execução em paralelo
-				for (int posicao = 0; posicao < 10; posicao++) {
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} // dá um tempo
-
-					// executando esse envio com um tempo de parada ou tempo determinado.
-					System.out.println("Executando uma rotina, envio de e-mail.");
-				}
-				// Fim do código em paralelo
-			}
-		}.start();// Start liga a thread que fica processando paralelamente por trás.
-
-		/*********************** DIVISÂO DAS THREADS ***********************/
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 
 		// Thread em processamento em paralelo do envio de nota fiscal
-		new Thread() {
+		Thread threadNotaFiscal =  new Thread(thread2);
+		threadNotaFiscal.start();
 
-			public void run() {
-
-				// Código da rotina para execução em paralelo 
-				for (int i = 0; i < 10; i++) {
-
-					// Quero excutar ess envio com tempo de parada, ou com tempo determinado
-					System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal!");
-					try {
-						Thread.sleep(4000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} // Tempo da thread
-
-				}
-				// Fim do código em paralelo 
-
-			};
-		}.start();// Start liga a thread que fica processando paralelamente
-		
 		// Código do sistema do usuário continua o fluxo de trabalho
 		System.out.println("FIM DA EXECUÇÂO DA THREAD!!!");
 		// Fluxo do sistema, cadastro de venda, uma emissão de nota fiscal
 		JOptionPane.showMessageDialog(null, "Sistema em execução para o usuário.");
 	}
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			// Código da rotina para execução em paralelo
+			for (int i = 0; i < 10; i++) {
+
+				// Quero excutar ess envio com tempo de parada, ou com tempo determinado
+				System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal!");
+				try {
+					Thread.sleep(4000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} // Tempo da thread
+			}
+			// Fim do código em paralelo
+		}
+	};
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			// Código da rotina para execução em paralelo
+			for (int posicao = 0; posicao < 10; posicao++) {
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} // dá um tempo
+
+				// executando esse envio com um tempo de parada ou tempo determinado.
+				System.out.println("Executando uma rotina, envio de e-mail.");
+			}
+			// Fim do código em paralelo
+		}
+	};
 }
