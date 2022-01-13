ddf3213557adfdac2829979345010712ee312786
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2022-01-12 21:15:03 -0300

    Criação de duas classes, ObjetoFilaThread e ImplementacaoFilaThread

---
+++ src/com/br/mclg/tela/ImplementacaoFilaThread.java
@@ -0,0 +1,41 @@
+package com.br.mclg.tela;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ImplementacaoFilaThread extends Thread {
+
+	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilha_fila = new ConcurrentLinkedQueue<ObjetoFilaThread>();
+
+	public static void add(ObjetoFilaThread objFilaThread) {
+		pilha_fila.add(objFilaThread);
+	}
+
+	@Override
+	public void run() {
+		Iterator iteracao = pilha_fila.iterator();
+
+		synchronized (iteracao) { // bloquea o acesso desta lista por outro processo
+			while (iteracao.hasNext()) { // enquanto conter dados na lista irá processar
+				ObjetoFilaThread processar = (ObjetoFilaThread) iteracao.next();// pega o objeto atual
+				
+				System.out.println("==========================================");
+				System.out.println(processar.getEmail());
+				System.out.println(processar.getNome());
+				
+				iteracao.remove();
+
+				try {
+					Thread.sleep(100);// dar um tempo pra descarga de memória
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		try {
+			Thread.sleep(1000);// processa toda a lista e, da um tempo pra limpeza de memória
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
